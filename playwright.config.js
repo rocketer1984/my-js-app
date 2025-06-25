9a94e35bd03a4c6eaccd4a258fd0e6d3dba1f41f
Cesar <rocketer1984@gmail.com>
2025-06-25 14:29:11 -0400

    Set headless true

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: false,               // Run tests without opening the browser
+    headless: true,               // Run tests without opening the browser
     viewport: { width: 1280, height: 720 },
     ignoreHTTPSErrors: true,
   },
